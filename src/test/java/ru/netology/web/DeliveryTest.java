1231c22cba8c04c9fa0ba786078e5e8efeb068b0
LessiaSa <0162221@mail.ru>
2024-08-19 15:27:31 +0700

    NewTest

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -35,7 +35,7 @@ public class DeliveryTest {
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
         $("[data-test-id=success-notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $("div.notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
         $("[data-test-id=success-notification] .icon-button__text").click();
         $("[data-test-id=date] .input__control").doubleClick().sendKeys(secondMeetingDate);
         $(".button__content").click();
@@ -43,6 +43,6 @@ public class DeliveryTest {
         $("[data-test-id=replan-notification] .notification__content").shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $("[data-test-id=replan-notification] .button__text").click();
         $("[data-test-id=success-notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $("div.notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
